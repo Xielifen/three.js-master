eea4012ac166feedd7803ec3b291caed6ee8ceec
xie <1418675724@qq.com>
2021-11-19 22:30:34 +0800

    add

---
+++ src/resources/textures.js
@@ -0,0 +1,62 @@
+//billboardTextures
+let billboardTextures = {};
+billboardTextures.terpSolutionsTexture = '../src/jsm/1.png';
+billboardTextures.bagHolderBetsTexture ='../src/jsm/2.png';
+billboardTextures.homeSweetHomeTexture ='../src/jsm/1.jpg';
+billboardTextures.textTexture="../src/jsm/1.jpg";
+
+//box textures
+let boxTexture = {};
+boxTexture.Github = '../src/jsm/githubLogo.png';
+boxTexture.twitter = '../src/jsm/twitter.png';
+boxTexture.LinkedIn = '../src/jsm/linkedInLogo.png';
+boxTexture.mail = '../src/jsm/envelope.png';
+boxTexture.globe = '../src/jsm/thunder.png';
+boxTexture.reactIcon = '../src/jsm/react.png';
+boxTexture.allSkills = '../src/jsm/allSkills.png';
+boxTexture.lensFlareMain = '../src/jsm/lensflare0.png';
+boxTexture.skrillex = '../src/jsm/skrillex.png';
+boxTexture.edmText = '../src/jsm/EDM.png';
+boxTexture.writing = '../src/jsm/writing.png';
+
+//material textures
+let stoneTexture = '../src/jsm/stone.png';
+let woodTexture = '../src/jsm/woodTexture.jpg';
+
+//text
+let inputText = {};
+//inputText.terpSolutionsText = '../src/jsm/terp-solutions-text.png';
+//inputText.activities = '../src/jsm/activities_text.png';
+//inputText.bagholderBetsText = '../src/jsm/bagholderbets-text.png';
+//inputText.homeSweetHomeText = '../src/jsm/1.jpg';
+//inputText.staticPortfolio = '../src/jsm/static-portfolio.png';
+
+//SVG
+let SVG = {};
+SVG.reactLogo = '../src/jsm/react-svg.svg';
+
+//URLs
+let URL = {};
+URL.terpsolutions =
+  'https://j1am1ng.github.io/warehouseManagement/';
+URL.githubBagholder= 'https://lalbj.github.io/ali-fe/index.html';
+/*URL.ryanfloyd = 'https://ryanfloyd.io';
+
+URL.homeSweetHomeURL = 'https://github.com/MrRyanFloyd/home-sweet-127.0.0.1';
+URL.gitHub = 'https://github.com/MrRyanFloyd/Portfolio_2020';
+URL.twitter = 'http://m.baidu.com';
+URL.LinkedIn = 'https://www.linkedin.com/in/ryan-floyd/';
+URL.email = 'https://mailto:arfloyd7@gmail.com';
+URL.githubBagholder = 'https://github.com/MrRyanFloyd/bagholder-bets';
+URL.githubHomeSweetHome = 'https://github.com/MrRyanFloyd/home-sweet-127.0.0.1';
+URL.devTo =
+  'https://dev.to/mrryanfloyd/create-an-interactive-3d-portfolio-website-that-stands-out-to-employers-47gc';
+*/
+export {
+  billboardTextures,
+  boxTexture,
+  inputText,
+  URL,
+  stoneTexture,
+  woodTexture,
+};
